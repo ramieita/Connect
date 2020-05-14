3e338b944401e632af88309266193014ae1f6699
Hamzah Fayad <hamzahfayad99@gmail.com>
2020-05-14 12:59:20 +0200

    mongodb, User-Schema
    *description*
    added mongodb connection and User Schema
    
    *Issue:* #01

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,36 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const configDB = require("./config/keys");
+
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
+
+app.use(bodyParser.json());
+app.use(cors());
+
+const db = configDB.DB;
+
+mongoose
+  .connect(db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log(`Connection to ${db} successful.`);
+  })
+  .catch((err) => {
+    console.log(`Connection to ${db} failed due to ${err}`);
+  });
 
 const PORT = 3000 || process.env.PORT;
 
 app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
-})
+  console.log(`Server is listening on port ${PORT}`);
+});
---
+++ backend/modal/User.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true 
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    pasword: {
+        type: String,
+        required: true 
+    },
+})
+
+module.exports = mongoose.model('User', userSchema);
