5e2313c889e7448fba9e667f6ca3ebeb7b2c5ff8
Hamzah Fayad <hamzahfayad99@gmail.com>
2020-05-20 13:55:55 +0200

    adding routes for users & profile *description* added routing for GET /users, DELETE /user/:d, PUT /profile
    *Issue:* #02.3

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -8,7 +8,8 @@ const key = require("../config/keys");
 /***** User *****/
 
 // Create User
-// /signup
+
+//Signup user
 router.post("/signup", (req, res) => {
   var username = req.body.username;
   var email = req.body.email;
@@ -37,15 +38,13 @@ router.post("/signup", (req, res) => {
         return res.status(400).json({
           message: "Email already exists.",
         });
-      }
-      else {
+      } else {
         //save new User
         let newUser = new User({
           username: username,
           email: email,
           password: password,
         });
-
         //hash password before saving new user into database
         //source: https://www.npmjs.com/package/bcrypt
         bcrypt.genSalt(10, function (err, salt) {
@@ -54,26 +53,10 @@ router.post("/signup", (req, res) => {
             newUser.password = hash;
             newUser.save().then(() => {
               User.findOne({ username: username }).then((user) => {
-                /*if (user !== null) {
-                  let jwtData = {
-                    id: user._id,
-                  };*/
-                  // sign jsonwebtoken and set token with users' id
-                  /*jwt.sign(
-                    { User: jwtData },
-                    "secretKey",
-                    { algorithm: "HS256" },
-                    (err, token) => {
-                      res.json({
-                        token: token,
-                      });*/
-                      return res.status(201).json({
-                        message: "User signed up successfully.",
-                        success: true,
-                      });
-                    //}
-                  //);
-                //}
+                return res.status(201).json({
+                  message: "User signed up successfully.",
+                  success: true,
+                });
               });
             });
           });
@@ -83,60 +66,127 @@ router.post("/signup", (req, res) => {
   });
 });
 
-// /login
+//Login user
 router.post("/login", (req, res) => {
-  User.findOne({username: req.body.username, email: req.body.email}, (err, user) => {
-    if(err) {
-      return res.status(500).json({
-        message: "Error caused while logging in"
-      });
-    }
-    if(!user) {
-      return res.status(401).json({
-        message: "User cannont be logged in. Check entered data."
-      });
-    }
-    if(!bcrypt.compareSync(req.body.password, user.password)) {
-      return res.status(401).json({
-        message: "Password is wrong. Login not successful."
-      })
-    }
-    else if(user != null) {
-      let jwData = {
-        id: user._id
+  User.findOne(
+    { username: req.body.username, email: req.body.email },
+    (err, user) => {
+      if (err) {
+        return res.status(500).json({
+          message: "Error caused while logging in",
+        });
       }
-      jwt.sign({User: jwData}, "secretkey", {algorithm: "HS256"}, (err, token) => {
-        res.status(200).json({
-          message: "Logged in successfully.",
-          token: token,
-          success: true,
-          id: user._id
-        })
-      })
+      if (!user) {
+        return res.status(401).json({
+          message: "User cannont be logged in. Check entered data.",
+        });
+      }
+      if (!bcrypt.compareSync(req.body.password, user.password)) {
+        return res.status(401).json({
+          message: "Password is wrong. Login not successful.",
+        });
+      } else if (user != null) {
+        let jwData = {
+          id: user._id,
+        };
+        jwt.sign(
+          { User: jwData },
+          "secretkey",
+          { algorithm: "HS256" },
+          (err, token) => {
+            res.status(200).json({
+              message: "Logged in successfully.",
+              token: token,
+              success: true,
+              id: user._id,
+            });
+          }
+        );
+      }
     }
-  })
+  );
 });
 
-router.get('/profile', verifytoken, (req, res) => {
+//GET profile data of user
+router.get("/profile", verifytoken, (req, res) => {
   jwt.verify(req.token, key.secretKey, (err, auth) => {
-      if(err) {
-          res.status(403).json({
-            message: "Access Forbidden"
-          }); 
-      } 
-      else {
-          User
-          .findOne({_id: auth.User.id})
-          .then((userProfile) => {
-              res.json({
-                  username: userProfile.username,
-                  email: userProfile.email
-              })
-          })
-      }
-  })
+    if (err) {
+      res.status(403).json({
+        message: "Access Forbidden",
+      });
+    } else {
+      User.findOne({ _id: auth.User.id }).then((userProfile) => {
+        res.json({
+          username: userProfile.username,
+          email: userProfile.email,
+        });
+      });
+    }
+  });
 });
 
+//Edit profile data
+router.put("/profile", verifytoken, (req, res) => {
+  jwt.verify(req.token, key.secretKey, (err, auth) => {
+    if (err) {
+      res.sendStatus(403);
+    } else {
+      User.findOneAndUpdate(
+        { _id: auth.User.id },
+        {
+          username: req.body.username,
+          email: req.body.email,
+        },
+        { new: true, safe: true }
+      ).then((updated) => {
+        if (updated) {
+          res.status(202).json({
+            message: "Updated successfully",
+            updated: true,
+          });
+        }
+      });
+    }
+  });
+});
 
+// GET all users
+router.get("/", (req, res) => {
+  User.find()
+    .then((users) => {
+      if (!users) {
+        return res.sendStatus(400).end();
+      }
+      res.send(users);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+//GET one user
+router.get("/:id", (req, res) => {
+  User.findById({ _id: req.params.id }).then((user) => {
+    if (!user) {
+      return res.sendStatus(404).end();
+    }
+    res.status(200).send(user);
+  });
+});
+
+//Delete user
+router.delete("/delete/:id", (req, res) => {
+  User.findByIdAndRemove({ _id: req.params.id })
+    .exec()
+    .then((user_deleted) => {
+      if (!user_deleted) {
+        return res.sendStatus(400).end();
+      }
+      return res.sendStatus(204).end();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
 module.exports = router;
