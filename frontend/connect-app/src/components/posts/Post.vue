80ace4ef5931d6b7fae60d63367e10716a245daf
Hamzah Fayad <hamzahfayad99@gmail.com>
2020-06-02 11:58:03 +0200

    Post.vue
    *description*
    prepare Post.vue in frontend
    *Issue*: #usestory-3

--- frontend/connect-app/src/components/posts/Post.vue
+++ frontend/connect-app/src/components/posts/Post.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
     <Navbar />Post Page
+    <p>List</p>
+    <li v-for="p in posts" :key="p.title">
+      {{ p.title }}
+      </li>
   </div>
 </template>
 
@@ -12,7 +16,34 @@ export default {
     postId: String
   },
   components: {
-      Navbar
-  }
+    Navbar
+  },
+  data() {
+    return {
+      posts: [
+        {
+          title: "Hi"
+        }
+      ]
+    };
+  },
+  /*mounted() {
+    let url =
+      'http://localhost:3000/api/v1/topic/'+ window.location.href.split("/")[5];
+      console.log(window.location.href.split("/")[5])
+    let headers = {
+      headers: { authorization: "Bearer " + localStorage.getItem("jwt") }
+    };
+    var self = this;
+    this.$http
+      .get(url, headers)
+      .then(res => {
+        self.posts = res.data.posts;
+        console.log(self.posts)
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }*/
 };
 </script>
