adbf85986a5bd1cf5403894105e40d3e63a7a793
Hamzah Fayad <hamzahfayad99@gmail.com>
2020-06-09 12:44:13 +0200

    add comments to master
    *description*
    prepare for comments
    *Issue:* #user_story_4

--- backend/modal/Comments.js
+++ backend/modal/Comments.js
@@ -1,26 +1,22 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const postSchema = new Schema({
-    topic: {
+const commentSchema = new Schema({
+    post: {
         type: Schema.Types.ObjectId,
-        ref: 'Topic',
+        ref: 'Post',
         required: true 
     },
-    title: {
-        type: String,
-        required: true 
-    },
-    content: {
+    commentContent: {
         type: String,
         required: true
     },
-    postedBy: {
+    commentedBy: {
         type: Schema.Types.ObjectId,
         ref: 'User',
         required: true
     },
-    comments: [{
+    subcomments: [{
         type: Schema.Types.ObjectId,
         ref: 'Comment',
         default: []
@@ -31,4 +27,4 @@ const postSchema = new Schema({
     }
 })
 
-module.exports = Post = mongoose.model('Post', postSchema);
+module.exports = Comment = mongoose.model('Comment', commentSchema);
